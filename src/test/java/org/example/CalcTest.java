5ed9ff3f8db5b85adc87a68f2ede727047374bc9
Nikolai2019 <kolya.ruslyakov@gmail.com>
2023-06-29 14:48:14 +0500

    Created Calculator: summ and remove. Created TestSuites for summ and remove.

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
 
